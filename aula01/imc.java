127197f3b435e30e9529df87cd0079c333281593
Rie Kanzato <mrie.kanzato@gmail.com>
2024-03-18 17:49:28 -0300

    aula sobre input e if

--- aula01/imc.java
+++ aula01/imc.java
@@ -21,9 +21,19 @@ class imc {
         float imc = peso/(altura * altura)
 
         if(imc < 16.9){
-            System.out.println(nome,"tem", altura,"de altura e pesa", peso, "kg. Sua situação é muito abaixo do peso");
-        } else if(imc > 17){
-            System.out.println(nome,"tem", altura,"de altura e pesa", peso, "kg. Sua situação é muito abaixo do peso");
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é muito abaixo do peso");
+        } else if(imc > 17 && imc < 18.4){
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é abaixo do peso");
+        } else if(imc > 18.5 && imc > 24.9){
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é normal");
+        } else if(imc > 25 && imc < 29.9){
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é acima do peso");
+        } else if(imc > 30 && imc < 34.9){
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é obesidade grau I");
+        } else if(imc > 35 && imc < 40){
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é obesidade grau II");
+        } else {
+            System.out.println(nome + "tem" + altura + "de altura e pesa" + peso + "kg. Sua situação é obesidae grau III");
         }
     }
 }
