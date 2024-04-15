6bc59e0cd241fe1692214ff0bcbe3da28ba4f05e
Rie Kanzato <mrie.kanzato@gmail.com>
2024-04-15 09:05:34 -0300

    aula sobre repetições

---
+++ aula03/Para.java
@@ -0,0 +1,9 @@
+package aula03;
+
+public class Para {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+        system.out.println(i);
+        }
+    }
+}
---
+++ aula03/Ternario.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class Ternario{
+    public static void main(String[]args){
+        int time = 20;
+        String result;
+        result = (time < 18) ? "good morning." : "good evening.";
+        System.out.println(result);
+    }
+}
